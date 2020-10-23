13f12294e6b2b724327fc9ef167b050185378a17
vasapa <ex@mail.com>
2020-10-23 10:18:45 +0300

    Блок stages

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,40 +6,18 @@ $(document).ready(function(){
 		// nav:true
 	});
 
-	// $('.carousel__inner').slick({
-  	// 	speed: 1200,
-	// 	// adaptiveHeight: true,
-	// 	prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.png"></button>',
-	// 	nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.png"></button>',
-	// 	responsive: [
-	// 		{
-	// 		  	breakpoint: 768,
-	// 		  	settings: {
-	// 				dots: true,
-	// 				arrows: false
-	// 		  	}
-	// 		}
-	// 	  ]
-	//   });
+	function toggleSlide(item){
+		$(item).each(function(i){
+			$(this).on('click', function(e){
+				e.preventDefault();
+				$('.stages__item-first').eq(i).toggleClass('stages__item-first_active');
+				$('.stages__item-second').eq(i).toggleClass('stages__item-second_active');
+			})
+		})
+	};
 
-	// $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-	// 	$(this)
-	// 	  	.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-	// 	  	.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-	// });
-
-	// function toggleSlide(item){
-	// 	$(item).each(function(i){
-	// 		$(this).on('click', function(e){
-	// 			e.preventDefault();
-	// 			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-	// 			$('.catalog-item__right').eq(i).toggleClass('catalog-item__right_active');
-	// 		})
-	// 	})
-	// };
-
-	// toggleSlide('.catalog-item__link');
-	// toggleSlide('.catalog-item__back');
+	toggleSlide('.stages__item-first');
+	toggleSlide('.stages__item-second');
 
 	// // Modal
 
@@ -120,20 +98,3 @@ $(document).ready(function(){
 	// new WOW().init();
 
 });
-
-// const slider = tns({
-//     container: '.carousel__inner',
-//     items: 1,
-//     slideBy: 'page',
-// 	autoplay: false,
-// 	controls: false,
-// 	navPosition: 'bottom'
-// });
-
-// document.querySelector('.prev').addEventListener('click', function (){
-// 	slider.goTo('prev');
-// });
-
-// document.querySelector('.next').addEventListener('click', function (){
-// 	slider.goTo('next');
-// });
